0702fb1b3f450314ced091bb41f3c8ed3a9c4be9
ashishagarwal2101 <ashish21.01agarwal@gmail.com>
2019-08-03 16:00:27 +0530

    Initial files

---
+++ android/app/src/main/java/com/dunzoapp_bootstrapparadox/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dunzoapp_bootstrapparadox;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "DunzoApp_BootstrapParadox";
+    }
+}
